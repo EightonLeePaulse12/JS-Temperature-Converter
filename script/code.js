69d5bbce069bd93a71618a1ec2d4fd35e95c4550
EightonLeePaulse12 <eightonleepaulse@gmail.com>
2023-06-08 14:56:52 +0200

    FIXED

--- script/code.js
+++ script/code.js
@@ -1,43 +1,41 @@
 // Defining variables
 
-const amount = document.querySelector('#enterAmount');
+const amount = document.querySelector("#enterAmount");
 const celString = "°C";
 const farString = "°F";
-const range = document.querySelector('#choose');
-const tempInput = document.querySelector('#enterAmount');
-const button = document.querySelector('#calculate')
-const answer = document.querySelector('#myAnswer')
-const clear = document.querySelector('#clear')
+const range = document.querySelector("#choose");
+const tempInput = document.querySelector("#enterAmount");
+const button = document.querySelector("#calculate");
+const answer = document.querySelector("#myAnswer");
+const clear = document.querySelector("#clear");
 
 // Functions
 
-button.addEventListener('click',()=>{
-    
-    let toFarhenheit = tempInput.value * 9 / 5 + 32;
-    let toCelsius = (tempInput.value*5/9 -32).toFixed(2);
+button.addEventListener("click", () => {
+  let toFarhenheit = (tempInput.value * 9) / 5 + 32;
+  let toCelsius = ((tempInput.value - 32) *  5/9).toFixed(2);
 
-    if (range.checked){
-        let finalAnswer = parseFloat(toCelsius)
-        answer.textContent = parseFloat(finalAnswer) + "°C"
-    } else{
-        let finalAnswer2 = parseFloat(toFarhenheit)
-        answer.textContent = parseFloat(finalAnswer2) + "°F"
-}
-})
+  if (range.checked) {
+    let finalAnswer = parseFloat(toCelsius);
+    answer.textContent = parseFloat(finalAnswer) + "°C";
+  } else {
+    let finalAnswer2 = parseFloat(toFarhenheit);
+    answer.textContent = parseFloat(finalAnswer2) + "°F";
+  }
+});
 
-clear.addEventListener('click',()=>{
-    if(tempInput.value = tempInput.value){
-        tempInput.value = ''
-    } else{
-        tempInput.value = tempInput.value 
-    }
+clear.addEventListener("click", () => {
+  if (tempInput.value === tempInput.value) {
+    tempInput.value = "";
+  } else {
+    tempInput.value = tempInput.value;
+  }
 
-    if(answer.textContent = answer.textContent){
-        answer.textContent = ''
-    }else{
-        answer.textContent = answer.textContent
-    }
-})
+  if (answer.textContent === answer.textContent) {
+    answer.textContent = "";
+  } else {
+    answer.textContent = answer.textContent;
+  }
+});
 
-
-console.log(answer.innerHTML)
+console.log(answer.innerHTML);
